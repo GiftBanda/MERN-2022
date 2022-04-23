9179484c690ba2eb8e17c37ee25320fd1e1fca59
GiftBanda <bandagift42@gmail.com>
2022-04-23 15:14:09 +0200

    added cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const colors = require('colors');
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware');
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
